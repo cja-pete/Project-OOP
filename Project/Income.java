04b36a8dfd194a5cdfd1032e17f153907f1bf52b
Pete <cja.chotipon@gmail.com>
2026-03-03 14:21:49 +0700

    Updata only Income.java

--- Project/Income.java
+++ Project/Income.java
@@ -1,3 +1,16 @@
 public class Income {
-    
+    private double salary;          // เงินเดือน 40(1)
+    private double otherEarnings;   // รายได้อื่น ๆ 40(1)
+
+    public double getTotalIncome() {
+        return this.salary + this.otherEarnings;
+    }
+
+    public double getSalary(double salary) {
+        return this.salary= salary;
+    }
+
+    public double getOtherEarnings(double otherEarnings) {
+        return this.otherEarnings = otherEarnings;
+    }
 }
